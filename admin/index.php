e3bba889e168e11d7270c3a8fe724a52d1d70a70
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-03-30 11:54:55 -0300

    feat: :sparkles: página inicial da área do administrador

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>English Portal - Admin Area</title>
+</head>
+<body>
+
+  <form action="auth/validacao.php" method="post">
+  <fieldset>
+  <legend>Admin Login</legend>
+      <label for="txUsuario">User</label>
+      <input type="text" name="usuario" id="txUsuario" maxlength="25" />
+      <label for="txSenha">Password</label>
+      <input type="password" name="senha" id="txSenha" />
+
+      <input type="submit" value="Enter" />
+  </fieldset>
+  </form>
+  
+</body>
+</html>
