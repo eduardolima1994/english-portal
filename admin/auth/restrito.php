b19e31f3bd095656ce4d7d6a98dbf1fdf0491d56
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-03-30 15:54:15 -0300

    refactor: :white_check_mark: melhoria de segurança e visibilidade na página restrita do administrador

--- admin/auth/restrito.php
+++ admin/auth/restrito.php
@@ -1,15 +1,12 @@
+<title>English Portal - Admin Area</title>
+
 <?php
 
   if (!isset($_SESSION)) session_start();
-
   $nivel_necessario = 2;
-
-  // Verifica se não há a variável da sessão que identifica o usuário
   if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] <$nivel_necessario)) {
-      // Destrói a sessão por segurança
       session_destroy();
-      // Redireciona o visitante de volta pro login
-      header("Location: ../index.php"); exit;
+      header("Location: ../"); exit;
   }
 
 ?>
@@ -18,16 +15,13 @@
   <p>Welcome, <?php echo $_SESSION['UsuarioNome']; ?>!</p>
 
 <?php
-
   $success = mysqli_connect("localhost", "root", "root", "english-portal") or trigger_error(mysql_error());
-
-  // executa a query
   $dados = mysqli_query($success, "SELECT * FROM `users`") or die(mysqli_error());
-  // transforma os dados em um array
   $linha = mysqli_fetch_assoc($dados);
-  // calcula quantos dados retornaram
   $total = mysqli_num_rows($dados);
 ?>
+
+<a href="../create/create.php">Create user</a>
 
 <table border="1px">
 	<caption>Account Management:</caption>
@@ -39,16 +33,14 @@
 		<th>Level</th>
 		<th>Active</th>
 		<th>Register</th>
-		<th>Alterar</th>
-    <th>Deletar</th>
+		<th>Alter</th>
+    <th>Delete</th>
 	</tr>
 	</thead>
 	<tbody>
 
 <?php
-	// se o número de resultados for maior que zero, mostra os dados
 	if($total > 0) {
-		// inicia o loop que vai mostrar todos os dados
 		do {
 ?>
         
@@ -71,18 +63,24 @@
       }
     ?></td>
 		<td><?=$linha['register']?></td>
-		<td>[X]</td>
-		<td>[X]</td>
+		<td><a href="#">[Alter]</a></td>
+		<td><?php
+      if($linha['level'] == 2){
+        echo '-';
+      }else{
+        ?>
+        <a href="../delete/delete.php?id=<?=$linha['id']?>" onclick="return confirm('Do you really want to delete the student?');">Delete</a>
+      <?php
+      }
+    ?></td>
 	</tr>	
-
+  
 <?php
-		// finaliza o loop que vai mostrar os dados
 		}while($linha = mysqli_fetch_assoc($dados));
-	// fim do if
 	}
 ?>
 
 </tbody>
 </table>
 
-<p><a href="logout.php">Voltar</a></p>
+<p><a href="logout.php">Logout</a></p>
