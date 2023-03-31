dd561d1d2b956a8be49a01b960bf5689edf29687
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-03-31 12:46:25 -0300

    refactor: :recycle: arquivos renomeados para o padrão em inglês

--- admin/auth/validation.php
+++ admin/auth/validation.php
@@ -1,36 +1,31 @@
 <?php
 
-  // Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
   if (!empty($_POST) AND (empty($_POST['usuario']) OR empty($_POST['senha']))) {
-      header("Location: ../index.php"); exit;
+      header("Location: ../"); exit;
   }
 
-  // Tenta se conectar ao servidor MySQL
   $success = mysqli_connect("localhost", "root", "root", "english-portal") or trigger_error(mysql_error());
 
   $user = $_POST['usuario'];
   $password = $_POST['senha'];
 
-  // Validação do usuário/senha digitados
   $query = mysqli_query($success, "SELECT `id`, `name`, `level` FROM `users` WHERE (`user` = '".$user ."') AND (`password` = '". sha1($password) ."') AND (`active` = 1) LIMIT 1");
 
   if (mysqli_num_rows($query) != 1) {
-      // Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado
+
       echo "Login inválido!"; exit;
-  } else {
-      // Salva os dados encontrados na variável $resultado
+
+    } else {
+
       $resultado = mysqli_fetch_assoc($query);
 
-      // Se a sessão não existir, inicia uma
       if (!isset($_SESSION)) session_start();
 
-      // Salva os dados encontrados na sessão
       $_SESSION['UsuarioID'] = $resultado['id'];
       $_SESSION['UsuarioNome'] = $resultado['name'];
       $_SESSION['UsuarioNivel'] = $resultado['level'];
 
-      // Redireciona o visitante
-      header("Location: restrito.php"); exit;
+      header("Location: restricted.php"); exit;
   }
 
 ?>
