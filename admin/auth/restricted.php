a3de03a772e0c962546ff819791b79ee7749938d
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-04-02 11:31:49 -0300

    refactor: :sparkles: importação de arquivo de sessão

--- admin/auth/restricted.php
+++ admin/auth/restricted.php
@@ -2,12 +2,7 @@
 
 <?php
 
-  if (!isset($_SESSION)) session_start();
-  $nivel_necessario = 2;
-  if (!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] <$nivel_necessario)) {
-      session_destroy();
-      header("Location: ../"); exit;
-  }
+  require('../auth/session.php');
 
 ?>
 
