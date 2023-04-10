3cea0c25005e89a0d069bdb9b8dd447c9400fffb
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-04-10 15:24:16 -0300

    feat: :zap: criação de arquivo para conexão com o banco de dados

--- admin/auth/connection.php
+++ admin/auth/connection.php
@@ -1,8 +1,14 @@
 <?php
-  $servername = "localhost";
-  $database = "english-portal";
+
+  $host = "localhost";
+  $dbname = "english-portal";
   $username = "root";
   $password = "root";
+  
+  $conn = new mysqli($host, $username, $password, $dbname);
+  if ($conn->connect_errno) {
+    echo "Falha ao conectar: " . $conn->connect_error;
+    exit();
+  }
 
-  $conn = mysqli_connect($servername, $username, $password, $database);
 ?>
