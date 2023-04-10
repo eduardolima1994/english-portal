b249fcc9f44dc0e05b06486ead3f0f1b81519140
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-04-10 10:46:34 -0300

    feat: :zap: criação de lógica para temperatura do dashboard

---
+++ admin/temperature/temperature.php
@@ -0,0 +1,22 @@
+<?php
+
+  $geolocation = json_decode(file_get_contents('http://ip-api.com/json/'));
+  $latitude = $geolocation->lat;
+  $longitude = $geolocation->lon;
+  $country = $geolocation->country;
+
+  $apiKey = "30170f94e5d7d16ac41dc71f622070d1";
+  $url = "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey";
+  $data1 = json_decode(file_get_contents($url));
+
+  if (isset($data1->name)) {
+    $cityName = $data1->name;
+  } else {
+    echo "Não foi possível obter informações sobre sua cidade atual.";
+  }
+
+  $response = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q={$cityName}&appid={$apiKey}");
+  $data2 = json_decode($response);
+  $temp = intval($data2->main->temp - 273.15);
+
+?>
