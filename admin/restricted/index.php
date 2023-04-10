10c6c66726c9ef1a0e69efd6defe00a8450c8fdc
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-04-10 14:13:32 -0300

    refactor: :zap: remoção de espaçamento desnecessário

--- admin/restricted/index.php
+++ admin/restricted/index.php
@@ -21,15 +21,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>English Portal - Admin Area - Welcome <?php echo $_SESSION['UsuarioNome']; ?>!</title>
   
-  
-  
-  
   <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
-  
-  
-  
-  
-  
   <!-- plugins:css -->
   <link rel="stylesheet" href="vendors/feather/feather.css">
   <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
