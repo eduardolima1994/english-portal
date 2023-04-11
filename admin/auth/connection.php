1cf9118ce61032036c31b6aade8f5bd4493f8983
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-04-11 11:38:16 -0300

    refactor: :lock: melhoria na segurança do sistema usando .env

--- admin/auth/connection.php
+++ admin/auth/connection.php
@@ -1,13 +1,21 @@
 <?php
 
-  $host = "localhost";
-  $dbname = "english-portal";
-  $username = "root";
-  $password = "root";
+  require realpath(__DIR__ . '/../../vendor/autoload.php');
+  $env_path = realpath(__DIR__ . '/../../.env');
+  if (!file_exists($env_path)) {
+    die('.env file not found');
+  }
+  $dotenv = Dotenv\Dotenv::createImmutable(dirname($env_path));
+  $dotenv->load();
+
+  $host = $_ENV['DB_HOST'];
+  $username = $_ENV['DB_USERNAME'];
+  $password = $_ENV['DB_PASSWORD'];
+  $dbname = $_ENV['DB_DATABASE'];
   
   $conn = new mysqli($host, $username, $password, $dbname);
   if ($conn->connect_errno) {
-    echo "Falha ao conectar: " . $conn->connect_error;
+    echo "Failed to connect. " . $conn->connect_error;
     exit();
   }
 
