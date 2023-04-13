9eb7898200753cecee7f2ab2e76d6ee99e83ad09
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-04-13 15:16:07 -0300

    refactor: :art: personalização da página inicial

--- index.php
+++ index.php
@@ -4,12 +4,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
   <title>English Portal</title>
 </head>
 <body>
-  <h3>Welcome to the English student support system!</h3>
+  <!--<h3>Welcome to the English student support system!</h3>
   <h3>Select an option:</h3>
   <p><a href="student/">Student Area</a></p>
-  <p><a href="admin/">Admin Area</a></p>
+  <p><a href="admin/">Admin Area</a></p>-->
+  
+    <div class="top-bar-container">
+      <div class="top-bar-fixed">
+        <div class="top-bar-content">
+
+          <div class="text-box">
+              <a href="#" class="btn btn-white btn-animate">Student</a>
+          </div>
+          <div class="text-box">
+              <a href="#" class="btn btn-white btn-animate">Teacher</a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  
+  <img class="imagem-adaptavel" src="images/englishPortal.png" alt="English Portal">
 </body>
 </html>
