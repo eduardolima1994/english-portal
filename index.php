c4dea7ef8dc82f897dead9a7b901e160fd1715fb
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-04-14 09:25:50 -0300

    refactor: :sparkles: alteração em links e icone da página inicial com banner

--- index.php
+++ index.php
@@ -5,28 +5,25 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="shortcut icon" href="images/favicon.ico" />
   <title>English Portal</title>
 </head>
 <body>
-  <!--<h3>Welcome to the English student support system!</h3>
-  <h3>Select an option:</h3>
-  <p><a href="student/">Student Area</a></p>
-  <p><a href="admin/">Admin Area</a></p>-->
   
-    <div class="top-bar-container">
-      <div class="top-bar-fixed">
-        <div class="top-bar-content">
+  <div class="top-bar-container">
+    <div class="top-bar-fixed">
+      <div class="top-bar-content">
 
-          <div class="text-box">
-              <a href="#" class="btn btn-white btn-animate">Student</a>
-          </div>
-          <div class="text-box">
-              <a href="#" class="btn btn-white btn-animate">Teacher</a>
-          </div>
-
+        <div class="text-box">
+            <a href="student" class="btn btn-white btn-animate">Student</a>
         </div>
+        <div class="text-box">
+            <a href="admin" class="btn btn-white btn-animate">Teacher</a>
+        </div>
+
       </div>
     </div>
+  </div>
   
   <img class="imagem-adaptavel" src="images/englishPortal.png" alt="English Portal">
 </body>
