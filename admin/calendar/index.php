1be6c91cbea5c8c2ff08be4e80e9f41704e8c4e6
Eduardo Lima <eduardolima.tecnico@gmail.com>
2023-04-18 09:28:53 -0300

    refactor: :zap: melhorias em página para exibição do calendário

--- admin/calendar/index.php
+++ admin/calendar/index.php
@@ -1,103 +1,249 @@
 <?php
 
-    require('../auth/session.php');
+  require('../auth/session.php');
+  require('../auth/connection.php');
+  $id = $_REQUEST['id'];
 
-    $id = $_REQUEST['id'];
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
+
 <head>
-    <meta charset="utf-8">
-    <meta lang="pt-BR">
-    <title>English Portal - Admin Area - Welcome <?php echo $_SESSION['UsuarioNome']; ?>!</title>
-    
-    <link rel='stylesheet' href='fullcalendar/fullcalendar.css' />
-    <script src='fullcalendar/lib/jquery.min.js'></script>
-    <script src='fullcalendar/lib/moment.min.js'></script>
-    <script src='fullcalendar/fullcalendar.js'></script>
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="../../images/favicon.ico" />
-    
-    <!-- script de tradução -->
-    <script src='fullcalendar/lang/en-ca.js'></script>
-        
-    <script>
-
-        function getCurrentDate() {
-            const date = new Date();
-            const year = date.getFullYear();
-            const month = String(date.getMonth() + 1).padStart(2, '0');
-            const day = String(date.getDate()).padStart(2, '0');
-            
-            return `${year}-${month}-${day}`;
-        }
-
-       $(document).ready(function() {	
-           	
-            //CARREGA CALENDÁRIO E EVENTOS DO BANCO
-            $('#calendario').fullCalendar({
-                theme: false,
-                eventClick: function(event) {
-                    window.open(`selectEvent.php?id=<?=$id?>&idEvent=${event.id}`, 'page_open', 'width=700,height=600');
-                },
-                buttonIcons: true,
-				weekNumbers: false,
-                header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,agendaWeek,agendaDay'
-                },
-                defaultDate: getCurrentDate(),
-                editable: true,
-                eventLimit: true, 
-                events: 'events.php',           
-                eventColor: '#c60b1e'
-            });	
-            
-            //CADASTRA NOVO EVENTO
-            $('#novo_evento').submit(function(){
-                //serialize() junta todos os dados do form e deixa pronto pra ser enviado pelo ajax
-                var datas = jQuery(this).serialize();
-                $.ajax({
-                    type: "POST",
-                    url: "registerEvent.php?id=<?= $id?>",
-                    data: datas,
-                    success: function(date)
-                    {   
-                        if(date == "1"){
-                            alert("Successfully registered! ");
-                            //atualiza a página!
-                            location.reload();
-                        }else{
-                            alert("There was some problem! ");
-                        }
-                    }
-                });                
-                return false;
-            });	
-	   }); 
-                
-    </script>
-    
-    <style>
-        #calendario{
-            position: relative;
-            width: 70%;
-            margin: 0px auto;
-        }        
-    </style>
-    
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>English Portal - Admin Area - Welcome <?php echo $_SESSION['UsuarioNome']; ?>!</title>
+  
+  <link rel="stylesheet" href="../restricted/vendors/mdi/css/materialdesignicons.min.css">
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../restricted/vendors/feather/feather.css">
+  <link rel="stylesheet" href="../restricted/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="../restricted/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="../restricted/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+  <link rel="stylesheet" href="../restricted/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" type="text/css" href="../restricted/js/select.dataTables.min.css">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../restricted/css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../../images/favicon.ico" />
 </head>
-<body>    
-    <div id='calendario'>
-        <br/>
-        <form id="novo_evento" action="" method="post">
-            Name event: <input type="text" name="name" required/><br/><br/>  
-            Date event: <input type="datetime-local" name="date" required/><br/><br/>       
-            <button type="submit"> Create new event </button>
-        </form>
-        <a href="../restricted"><button> Back </button></a>
+<body>
+
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo mr-5" href="../restricted/"><img src="../restricted/../restricted/images/logo.png" class="mr-2" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="../restricted/"><img src="../restricted/../restricted/images/logo-mini.png" alt="logo"/></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="icon-menu"></span>
+        </button>
+        <ul class="navbar-nav mr-lg-2">
+          <li class="nav-item nav-search d-none d-lg-block">
+            <div class="input-group">
+              
+            </div>
+          </li>
+        </ul>
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item dropdown">
+            <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
+              <i class="icon-bell mx-0"></i>
+              <span class="count"></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+              <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
+              <a class="dropdown-item preview-item">
+                <div class="preview-thumbnail">
+                  <div class="preview-icon bg-success">
+                    <i class="ti-info-alt mx-0"></i>
+                  </div>
+                </div>
+                <div class="preview-item-content">
+                  <h6 class="preview-subject font-weight-normal">Welcome to the English Portal!</h6>
+                  <p class="font-weight-light small-text mb-0 text-muted">
+                    You bilingual is our main goal!
+                  </p>
+                </div>              
+              </a>
+              
+            </div>
+          </li>
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+              <img src="../restricted/images/faces/face1.jpg" alt="profile"/>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              <a class="dropdown-item">
+                <i class="ti-settings text-primary"></i>
+                Settings
+              </a>
+              <a class="dropdown-item" href="../auth/logout.php">
+                <i class="ti-power-off text-primary"></i>
+                Logout
+              </a>
+            </div>
+          </li>
+          <li class="nav-item nav-settings d-none d-lg-flex">
+            <a class="nav-link" href="#">
+              <i class="icon-ellipsis"></i>
+            </a>
+          </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+        </button>
+      </div>
+    </nav>
+    
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_settings-panel.html -->
+      <div class="theme-setting-wrapper">
+        <div id="settings-trigger"><i class="ti-settings"></i></div>
+        <div id="theme-settings" class="settings-panel">
+          <i class="settings-close ti-close"></i>
+          <p class="settings-heading">SIDEBAR SKINS</p>
+          <div class="sidebar-bg-options selected" id="sidebar-light-theme"><div class="img-ss rounded-circle bg-light border mr-3"></div>Light</div>
+          <div class="sidebar-bg-options" id="sidebar-dark-theme"><div class="img-ss rounded-circle bg-dark border mr-3"></div>Dark</div>
+          <p class="settings-heading mt-2">HEADER SKINS</p>
+          <div class="color-tiles mx-0 px-4">
+            <div class="tiles dark"></div>
+            <div class="tiles default"></div>
+          </div>
+        </div>
+      </div>
+      <div id="right-sidebar" class="settings-panel">
+        <i class="settings-close ti-close"></i>
+        <ul class="nav nav-tabs border-top" id="setting-panel" role="tablist">
+
+          <li class="nav-item">
+            <a class="nav-link" id="chats-tab" data-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="setting-content">
+
+          <div class="tab-pane fade show active scroll-wrapper" id="chats-section" role="tabpanel" aria-labelledby="chats-section">
+            <div class="d-flex align-items-center justify-content-between border-bottom">
+              <p class="settings-heading border-top-0 mb-3 pl-3 pt-0 border-bottom-0 pb-0">Friends</p>
+              <small class="settings-heading border-top-0 mb-3 pt-0 border-bottom-0 pb-0 pr-3 font-weight-normal">See All</small>
+            </div>
+            <ul class="chat-list">
+              <li class="list active">
+                <div class="profile"><img src="../restricted/images/faces/face0.png" alt="image"><span class="online"></span></div>
+                <div class="info">
+                  <p>Eduardo Lima</p>
+                  <p>Available</p>
+                </div>
+                <small class="text-muted my-auto">19 min</small>
+              </li>
+              <li class="list">
+                <div class="profile"><img src="images/faces/face0.png" alt="image"><span class="offline"></span></div>
+                <div class="info">
+                  <div class="wrapper d-flex">
+                    <p>Tifany Souza</p>
+                  </div>
+                  <p>Away</p>
+                </div>
+                <div class="badge badge-success badge-pill my-auto mx-2">4</div>
+                <small class="text-muted my-auto">23 min</small>
+              </li>
+              
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+      
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="../restricted/">
+              <i class="icon-grid menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">Users</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="#">Name</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Level</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Plus</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="icon-paper menu-icon"></i>
+              <span class="menu-title">Documentation</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+         
+          
+          <div class="col-lg-12 grid-margin stretch-card-calendar">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Calendar</h4>
+                  <?php
+                    echo "<iframe src='calendar.php?id=$id'></iframe>";
+                  ?>
+                </div>
+              </div>
+          </div>
+        </div>
+        
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023. English Portal by <a href="#" target="_blank">Jéssica Teacher</a>. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Developed by Eduardo Lima <i class="ti-heart text-danger ml-1"></i></span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
     </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="../restricted/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="../restricted/vendors/chart.js/Chart.min.js"></script>
+  <script src="../restricted/vendors/datatables.net/jquery.dataTables.js"></script>
+  <script src="../restricted/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+  <script src="../restricted/js/dataTables.select.min.js"></script>
+
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="../restricted/js/off-canvas.js"></script>
+  <script src="../restricted/js/hoverable-collapse.js"></script>
+  <script src="../restricted/js/template.js"></script>
+  <script src="../restricted/js/settings.js"></script>
+  <script src="../restricted/js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="../restricted/js/dashboard.js"></script>
+  <script src="../restricted/js/Chart.roundedBarCharts.js"></script>
+  <!-- End custom js for this page-->
 </body>
+
 </html>
+
